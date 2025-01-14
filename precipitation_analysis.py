3381c842e6b71d6b8fa13e53dcf113f5ff844822
trillianm <leilammmar@gmail.com>
2025-01-14 14:56:21 +0100

    Exercise 1 completed

--- precipitation_analysis.py
+++ precipitation_analysis.py
@@ -3,19 +3,23 @@ import json
 with open('precipitation.json') as file:
     precipitation_data = json.load(file)
 
-precipitation_seattle = {}
-total_monthly_precipitation = []
-list_index = 0
+precipitation_seattle = []
+total_monthly_precipitation = {}
+total_prec = 0
 
 for measurement in precipitation_data:
     if measurement["station"] == "GHCND:US1WAKG0038":
-        precipitation_seattle.update(precipitation_data(list_index))
-        # precipitation_seattle.update(measurement) 
-        list_index = list_index + 1
+        precipitation_seattle.append(measurement)
+
+for measurement in precipitation_seattle: 
+    months = int(measurement['date'].split('-')[1])
+    precipitation_value = measurement['value']
+    if months in total_monthly_precipitation:
+        total_monthly_precipitation[months] += precipitation_value
+    else:
+        total_monthly_precipitation[months] = precipitation_value    
 
 with open('precipitation_seattle.json', 'w', encoding='utf-8') as file:        
     json.dump(precipitation_seattle, file)
 
-# since the json file is a list of directories (might be worth asking about this), I was trying to get it to add the item of the list to a new list?
-
-
+ 
