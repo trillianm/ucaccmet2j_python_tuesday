388b600a2127697abab978f913b0b90623f836b4
trillianm <leilammmar@gmail.com>
2025-01-14 16:43:52 +0100

    Question 2 all

--- precipitation_analysis.py
+++ precipitation_analysis.py
@@ -1,8 +1,11 @@
 import json
+import csv
 
 with open('precipitation.json') as file:
     precipitation_data = json.load(file)
 
+
+# Question 1   
 precipitation_seattle = []
 total_monthly_precipitation = {}
 total_prec = 0
@@ -22,4 +25,28 @@ for measurement in precipitation_seattle:
 with open('total_monthly_precipitation.json', 'w', encoding='utf-8') as file:        
     json.dump(total_monthly_precipitation, file)
 
- 
+# Question 2 (started without realising that it was only meant to be only Seattle, so this is part of question 3 also)
+yearly_prec_cincinnati = 0
+yearly_prec_maui = 0
+yearly_prec_san_diego = 0
+yearly_prec_seattle = 0
+
+for measurement in precipitation_data:
+    if measurement["station"] == "GHCND:USW00093814":
+        yearly_prec_cincinnati += measurement['value']
+    elif measurement['station'] == "GHCND:USC00513317":
+        yearly_prec_maui += measurement['value']
+    elif measurement['station'] == "GHCND:US1CASD0032":
+        yearly_prec_san_diego += measurement['value']
+    else:
+        yearly_prec_seattle += measurement['value']
+
+#   seattle Question 2
+relative_monthly_prec = {}
+key = 1
+for measurement in total_monthly_precipitation:
+    relative_monthly_prec[key] = total_monthly_precipitation[key]/yearly_prec_seattle
+    key += 1
+
+with open('seattle_relative_precipitation.json', 'w', encoding='utf-8') as file:        
+    json.dump(relative_monthly_prec, file)
